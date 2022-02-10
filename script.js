d65b26de61af20f240f4c90265f4896d3496f94e
Eduardo Nagashima <eduyudji@gmail.com>
2022-02-09 21:14:53 -0300

    thiaguinho troll

---
+++ script.js
@@ -0,0 +1,11 @@
+let contador = 0;
+
+function diminuir() {
+    alert('KKKKKKK Não da pra diminuir as trolladas do Thiago!');
+}
+
+function aumentar() {
+    contador++;
+    let cont = document.querySelector('div span');
+    cont.innerHTML = contador;
+}
